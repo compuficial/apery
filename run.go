6957db662875b645c5fc7784ab529981361a7382
compuficial <compuficial@gmail.com>
2026-03-20 21:11:32 -0400

    feat: export plan loading, generator info, and new writer constructors

--- run.go
+++ run.go
@@ -2,9 +2,11 @@ package apery
 
 import (
 	"apery/internal/plan"
+	"apery/internal/registry"
 	"apery/internal/runtime"
 	"apery/internal/writer"
 	"context"
+	"io"
 )
 
 type (
@@ -18,10 +20,29 @@ type (
 
 	Logger = runtime.Logger
 	Option = runtime.Option
+
+	GeneratorInfo = registry.GeneratorInfo
+	ConfigKey     = registry.ConfigKey
 )
+
+func LoadPlanFile(path string) (*Plan, error) {
+	return plan.LoadFile(path)
+}
+
+func ListGenerators() []GeneratorInfo {
+	return registry.ListGenerators()
+}
 
 func WithLogger(logger Logger) Option {
 	return runtime.WithLogger(logger)
+}
+
+func WithWorkers(n int) Option {
+	return runtime.WithWorkers(n)
+}
+
+func WithChunkSize(n int64) Option {
+	return runtime.WithChunkSize(n)
 }
 
 func ValidatePlan(p *Plan) error {
@@ -34,6 +55,18 @@ func NewJSONLWriter(path string) (*writer.JSONLWriter, error) {
 
 func NewCSVWriter(path string) (*writer.CSVWriter, error) {
 	return writer.NewCSVWriter(path)
+}
+
+func NewJSONLWriterFromWriter(w io.Writer) *writer.JSONLWriter {
+	return writer.NewJSONLWriterFromWriter(w)
+}
+
+func NewCSVWriterFromWriter(w io.Writer) *writer.CSVWriter {
+	return writer.NewCSVWriterFromWriter(w)
+}
+
+func NewSplitWriter(dir, format string) *writer.SplitWriter {
+	return writer.NewSplitWriter(dir, format)
 }
 
 func Run(ctx context.Context, p *Plan, w Writer, opts ...Option) error {
